e45edf127fa4564c738aa16b350501cd973c19ac
Oleg Andrianov <andrianov.ov@yandex.ru>
2021-05-05 15:42:57 +0300

    внес правки form.html после проверки

--- source/js/app.js
+++ source/js/app.js
@@ -1,44 +1,45 @@
 let profileTags = document.querySelector('.profile__tags-list');
-profileTags.addEventListener("keyup", function () {
-  if (this.scrollTop > 0) {
-    this.style.height = this.scrollHeight + "px";
-  }
+let inputMinus = document.querySelectorAll('.settings__minus');
+let inputPlus = document.querySelectorAll('.settings__plus');
+let routeActive = document.querySelector('.route__item--active');
+let routeRemove = document.querySelector('.route__item--active .route__remove');
+let routeDropdown = document.querySelector('.route__dropdown');
+
+profileTags.addEventListener('keyup', (event) => {
+   let elem = event.currentTarget;
+   if (elem.scrollTop > 0) {
+      elem.style.height = `${elem.scrollHeight}px`;
+   }
 });
 
 
 
-let inputMinus = document.querySelectorAll(".settings__minus");
+
 inputMinus.forEach(item => {
-  item.addEventListener("click", () => {
-    let inputParent = item.closest(".settings__measure");
-    let input = inputParent.querySelector(".settings__input");
-    inputValue = input.value;
-    if (inputValue >= 1) {
-      input.value -= 1;
-    }
-  });
+   item.addEventListener('click', () => {
+      let inputParent = item.closest('.settings__measure');
+      let input = inputParent.querySelector('.settings__input');
+      if (input.value >= 1) {
+         input.value -= 1;
+      }
+   });
 });
 
-let inputPlus = document.querySelectorAll(".settings__plus");
+
 inputPlus.forEach(item => {
-  item.addEventListener("click", () => {
-    let inputParent = item.closest(".settings__measure");
-    let input = inputParent.querySelector(".settings__input");
-    inputValue = input.value;
-    input.value = +input.value + +1;
-  });
+   item.addEventListener('click', () => {
+      let inputParent = item.closest('.settings__measure');
+      let input = inputParent.querySelector('.settings__input');
+      input.value = +input.value + 1;
+   });
 });
 
 
-let routeActive = document.querySelector(".route__item--active");
-let routeRemove = document.querySelector(".route__item--active .route__remove");
-let routeDropdown = document.querySelector(".route__dropdown");
-routeActive.addEventListener("click", () => {
-  if (!(routeDropdown.classList.contains("route__dropdown--active"))) {
-    routeDropdown.classList.add("route__dropdown--active");
-    routeRemove.style.display = "none";
+
+routeActive.addEventListener('click', () => {
+  if (!(routeActive.classList.contains('route__item--opened'))) {
+    routeActive.classList.add('route__item--opened');
   } else {
-    routeDropdown.classList.remove("route__dropdown--active");
-    routeRemove.style.display = "block";
+    routeActive.classList.remove('route__item--opened');
   }
 });
